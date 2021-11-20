7bafb6f10744f6a97a1a19294be5f3af12cca187
lisbethchavez <chavezlisbeth639@gmail.com>
2021-11-19 20:52:04 -0500

    Agregar archivos de proyecto.

---
+++ CadenaDeCaracteresTar2/InvertirCadena.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace CadenaDeCaracteresTar2
+{
+    partial class frmInvertirCadena
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtContarPalabras = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Subway Circle DEMO", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(489, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TAREA INVERTIR Y CONTAR CADENA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Entrada de la Cadena:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salida de la Cadena:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(188, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(410, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(55, 371);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(208, 43);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "ACEPTAR";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(332, 373);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(240, 38);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contador de palabras:";
+            // 
+            // txtContarPalabras
+            // 
+            this.txtContarPalabras.Location = new System.Drawing.Point(192, 203);
+            this.txtContarPalabras.Name = "txtContarPalabras";
+            this.txtContarPalabras.Size = new System.Drawing.Size(162, 20);
+            this.txtContarPalabras.TabIndex = 8;
+            // 
+            // frmInvertirCadena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 478);
+            this.Controls.Add(this.txtContarPalabras);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmInvertirCadena";
+            this.Text = "INVERTIR CADENA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtContarPalabras;
+    }
+}
+
---
+++ CadenaDeCaracteresTar2/InvertirCadena.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadenaDeCaracteresTar2
+{
+    public partial class frmInvertirCadena : Form
+    {
+        public frmInvertirCadena()
+        {
+            InitializeComponent();
+        }
+        string original, salida;
+
+       
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            salida = invertircaracteres(original);
+            textBox2.Text = salida.ToString();
+
+
+            txtContarPalabras.Text = "" + textBox1.TextLength; 
+
+        }
+        public string invertircaracteres(string original)
+        {
+            string invierte = "";
+            for (int i = original.Length - 1; i >= 0; i--)
+                invierte += original[i];
+            return invierte;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            original = textBox1.Text;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
